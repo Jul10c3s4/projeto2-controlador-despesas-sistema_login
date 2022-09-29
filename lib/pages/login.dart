3e287603c905333bf6ea51553d3c0bf636c30bfa
Your Name <jcso2@aluno.ifal.edu.br>
2022-09-28 22:21:12 -0300

    tela_login

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -10,6 +10,8 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _senha = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -19,112 +21,185 @@ class _LoginState extends State<Login> {
         children: [
           Column(
             children: [
-              Padding(
+              Center(
+                child: Padding(
                 padding: EdgeInsets.symmetric(horizontal: 20),
                 child: Form(
                     child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    SizedBox(
-                      height: 50,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 24, vertical: 16),
-                          decoration: BoxDecoration(
-                              color: Colors.orangeAccent,
-                              borderRadius: BorderRadius.circular(20)),
-                          child: Text(
-                            "Login",
-                            style: TextStyle(
-                              fontSize: 25,
-                              fontWeight: FontWeight.w800,
-                              color: Colors.white,
+                    Container(
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.symmetric(vertical: 90),
+                      child: Card(
+                        color: Colors.white,
+                        elevation: 16,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 20),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                children: [
+                                  SizedBox(
+                                    height: 30,
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Container(
+                                        padding: EdgeInsets.symmetric(
+                                            horizontal: 24, vertical: 10),
+                                        decoration: BoxDecoration(
+                                            color: Colors.orangeAccent,
+                                            borderRadius:
+                                                BorderRadius.circular(20)),
+                                        child: Text(
+                                          "Login",
+                                          style: TextStyle(
+                                            fontSize: 25,
+                                            fontWeight: FontWeight.w800,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: 30,
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [],
+                                  ),
+                                  SizedBox(
+                                    height: 3,
+                                  ),
+                                  Container(
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10),
+                                        color: Colors.white),
+                                    child: TextField(
+                                      controller: _email,
+                                      keyboardType: TextInputType.emailAddress,
+                                      decoration: InputDecoration(
+                                          prefixIcon: Icon(
+                                            Icons.person,
+                                            color: Colors.black,
+                                          ),
+                                          label: Text(
+                                            "Email:",
+                                            style: TextStyle(
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.w500,
+                                              color: Colors.black,
+                                            ),
+                                          ),
+                                          hintText: "joao@gmail.com",
+                                          hintStyle: TextStyle(
+                                            fontSize: 20,
+                                            color: Colors.black45,
+                                          ),
+                                          border: OutlineInputBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                          )),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 20,
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [],
+                                  ),
+                                  SizedBox(
+                                    height: 3,
+                                  ),
+                                  Container(
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10),
+                                        color: Colors.white),
+                                    child: TextField(
+                                      controller: _senha,
+                                      obscureText: true,
+                                      keyboardType: TextInputType.emailAddress,
+                                      decoration: InputDecoration(
+                                          label: Text(
+                                            "Senha:",
+                                            style: TextStyle(
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.w500,
+                                              color: Colors.black,
+                                            ),
+                                          ),
+                                          suffixIcon: IconButton(
+                                            onPressed: () {},
+                                            icon: Icon(
+                                              Icons.remove_red_eye_rounded,
+                                              color: Colors.black,
+                                            ),
+                                          ),
+                                          prefixIcon: Icon(
+                                            Icons.https_rounded,
+                                            color: Colors.black,
+                                          ),
+                                          hintText: "*********",
+                                          hintStyle: TextStyle(
+                                            fontSize: 20,
+                                            color: Colors.black45,
+                                          ),
+                                          border: OutlineInputBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                          )),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 30,
+                                  ),
+                                  ElevatedButton(
+                                    onPressed: () {
+                                      _logar();
+                                    },
+                                    child: Text(
+                                      "Entrar",
+                                      style: TextStyle(
+                                        fontSize: 25,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    style: ElevatedButton.styleFrom(
+                                        backgroundColor: Colors.deepOrange,
+                                        padding:
+                                            EdgeInsets.symmetric(vertical: 10)),
+                                  ),
+                                  SizedBox(
+                                    height: 30,
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      height: 70,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Email:",
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.black,
-                          ),
+                          ],
                         ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 3,
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.white),
-                      child: TextField(
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: InputDecoration(
-                          hintText: "joao@gmail.com",
-                          hintStyle: TextStyle(
-                            fontSize: 20,
-                            color: Colors.black45,
-                          ),
-                          border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        )),
-                      ),
-                    ),
-                    SizedBox(height: 20,),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Senha:",
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 3,
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.white),
-                      child: TextField(
-                        obscureText: true,
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: InputDecoration(
-                          hintText: "*********",
-                          hintStyle: TextStyle(
-                            fontSize: 20,
-                            color: Colors.black45,
-                          ),
-                          border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        )),
                       ),
                     ),
                   ],
                 )),
               ),
+              )
+              
             ],
           )
         ],
       ),
     ));
+  }
+  void _logar(){
+    
   }
 }
